1f6ff9c388f6072770f1a269d6284891bcc55bd1
francoperez03 <franco.perez03@gmail.com>
2024-05-29 00:36:03 +0100

    feat: Decentralized Stablecoin finished

---
+++ src/DecentralizedStableCoin.sol
@@ -0,0 +1,67 @@
+//SPDX-License-Identifier: MIT
+// This is considered an Exogenous, Decentralized, Anchored (pegged), Crypto Collateralized low volitility coin
+
+// Layout of Contract:
+// version
+// imports
+// interfaces, libraries, contracts
+// errors
+// Type declarations
+// State variables
+// Events
+// Modifiers
+// Functions
+
+// Layout of Functions:
+// constructor
+// receive function (if exists)
+// fallback function (if exists)
+// external
+// public
+// internal
+// private
+// view & pure functions
+pragma solidity ^0.8.18;
+
+import {ERC20Burnable, ERC20} from "@openzeppelin/contracts/token/ERC20/extensions/ERC20Burnable.sol";
+import {Ownable} from "@openzeppelin/contracts/access/Ownable.sol";
+
+/**
+ * @title DecentralizedStableCoin
+ * @author Franco Perez
+ * Relative Stability: Pegged to US Dollar
+ * Stability Mechanism: Algorithmic
+ * Collateral Exogenous (wETH, wBTC)
+ * 
+ * This is the contract meant governed by DSCEngine
+ */
+contract DecentralizedStableCoin is ERC20Burnable, Ownable {
+  error DecentralizedStableCoin__MustBeMoreThanZero();
+  error DecentralizedStableCoin__BurnAmountExceedsBalance();
+  error DecentralizedStableCoin__NotZeroAddress();
+  constructor() ERC20("Decentralized Stablecoin", "DSC") Ownable(msg.sender) {
+  }
+
+  function burn(uint256 _amount) public override onlyOwner {
+    if(_amount <= 0) {
+      revert DecentralizedStableCoin__MustBeMoreThanZero();
+    }
+    uint256 balance = balanceOf(msg.sender);
+    if(balance <= _amount) {
+      revert DecentralizedStableCoin__BurnAmountExceedsBalance();
+    }
+    super.burn(_amount);
+  }
+
+  function mint(address _to, uint256 _amount) external onlyOwner returns (bool){
+    if(_amount <= 0) {
+      revert DecentralizedStableCoin__MustBeMoreThanZero();
+    }
+    if(_to == address(0)){
+      revert DecentralizedStableCoin__NotZeroAddress();
+    }
+    _mint(_to, _amount);
+    return true;
+  }
+  
+}
